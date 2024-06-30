24512d73bfbafec968460fb24d3c8931fd616324
Matteo Redaelli <matteo.redaelligmail.com>
2024-06-30 23:00:49 +0200

    updated ranks in teh website

--- dmcommunity-2024-06-six-marriages.pl
+++ dmcommunity-2024-06-six-marriages.pl
@@ -1,34 +1,44 @@
 %% https://dmcommunity.org/challenge/challenge-june-2024/
 
+%% This file is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+%%
+%% This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>. 
+
+%% Author matteo.redaelli@gmail.com
+
+%% solution
+%% findall(M, (sol(S), marriages(S, M)), L), maplist(writeln, L).
+
 :-use_module(library(clpfd)).
 
-dim(5).
+dim(6).
 
-husbands_ranks([
-		  [5,1,2,4,3],
-		  [4,1,3,2,5],
-		  [5,3,2,4,1],
-		  [1,5,4,3,2],
-		  [4,3,2,1,5]
-	      ]).
+husbands_ranks([[1,2,4,5,6,3],
+		[4,2,5,3,1,6],
+		[2,5,4,6,3,1],
+		[3,4,2,6,5,1],
+		[6,3,2,1,5,4],
+		[4,6,3,2,1,5]
+	       ]).
 
-wifes_ranks([
-		    [1,2,4,3,4],
-		    [3,5,1,2,4],
-		    [5,4,2,1,3],
-		    [1,4,3,2,5],
-		    [4,2,3,5,1]
-		]).
+wifes_ranks([[5,2,6,3,1,4],
+	     [6,5,1,2,4,3],
+	     [3,2,1,4,5,6],
+	     [2,4,1,3,5,6],
+	     [3,5,4,2,1,6],
+	     [5,1,6,4,2,3]
+	    ]).
 
 rank(Person1, Person2, Ranks, Rank):-
     nth1(Person1, Ranks, PersonRanks),
     nth1(Person2, PersonRanks, Rank).
 
 /*
-
 Wifes is a list of integer
 First element is the wife of men 1
-Second element is teh wife of men 2
+Second element is the wife of men 2
 ...
 */
 sol(Wifes):-
@@ -46,6 +56,7 @@ sol(Wifes):-
 instable_marriages(Wifes, HusbandRanks, WifeRanks, [Husband1-Wife1, Husband2-Wife2]):-
     %% Husband1 is the index of an element of the list
     %% Wife1 is a element of teh list of 
+    %% Wife1 is a element of teh list of 
     nth1(Husband1, Wifes, Wife1),
     rank(Husband1, Wife1, HusbandRanks, MRank11),
     nth1(Husband2, Wifes, Wife2),
@@ -61,8 +72,10 @@ instable_marriages(Wifes, HusbandRanks, WifeRanks, [Husband1-Wife1, Husband2-Wif
 stable_marriages(Wifes, HusbandsRanks, WifesRanks):-
     instable_marriages(Wifes, HusbandsRanks, WifesRanks, _) -> false ; true.
 
-husbands_names(['Adam','Bob','Charlie','Dave','Edgar']).
-wifes_names(['Alice','Barbara','Claire','Doris','Elsie']).
+%% needed to convert numbers to first names
+
+husbands_names(['Adam','Bob','Charlie','Dave','Edgar', 'Fred']).
+wifes_names(['Alice','Barbara','Claire','Doris','Elsie', 'Fiona']).
 
 wife_index_name(Index, Name):-
     wifes_names(WifesNames),
@@ -72,8 +85,7 @@ wifes_index_name([], []).
 wifes_index_name([Index|Indexes], [Name|Names]):-
     wife_index_name(Index, Name),
     wifes_index_name(Indexes, Names).
-
-
+    
 marriages(Wifes, Marriages):-
     husbands_names(HusbandsNames), 
     wifes_index_name(Wifes, WifesNames),
@@ -94,7 +106,6 @@ marriages_stats(H, [W|Wifes], HusbandRanks, WifeRanks, SumRanks):-
     NextHusband #= H + 1,
     marriages_stats(NextHusband, Wifes, HusbandRanks, WifeRanks, OtherSumRanks),
     SumRanks #= SumRanks1 + OtherSumRanks.
-
 
 marriages_stats(Wifes, SumRanks):-
     husbands_ranks(HusbandRanks),
